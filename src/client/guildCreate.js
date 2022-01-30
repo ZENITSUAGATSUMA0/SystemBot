843d72ce7033f7c12292de1082cf0872460aa1f2
Abdullah <69731556+marquis-dev@users.noreply.github.com>
2022-01-30 19:12:13 +0400

    Update guildCreate.js

--- src/client/guildCreate.js
+++ src/client/guildCreate.js
@@ -1,6 +1,6 @@
 module.exports = async (client, guild) => {
     try {
         await guild.commands.set(client.slash)
-    } catch (err) { console.log('GuildCreate_Error: Cannot create slash commands on', guild.name, guild.id) }
+    } catch (err) { console.log('GuildCreate_Error: Cannot create slash commands on', guild.name, guild.id);console.log(`Supported Invite Link: https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot%20applications.commands`) }
 }
 // Layer Coding : !                  Marquis#9999
