f20405e8bb0bf5d0b18d357f9cb7744c8245c61e
Abdullah <69731556+marquis-dev@users.noreply.github.com>
2022-01-30 19:11:46 +0400

    Update ready.js

--- src/client/ready.js
+++ src/client/ready.js
@@ -9,7 +9,7 @@ module.exports = async (client) => {
     client.guilds.cache.forEach(async guild => {
         try {
             await guild.commands.set(client.slash)
-        } catch(err) { console.log('Ready_Error: Cannot create slash commands on', guild.name, guild.id, err) }
+        } catch(err) { console.log('Ready_Error: Cannot create slash commands on', guild.name, guild.id);console.log(`Supported Invite Link: https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot%20applications.commands`) }
     });
     
 }
