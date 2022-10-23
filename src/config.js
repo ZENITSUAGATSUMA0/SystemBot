e3bf1b8ee6a65aa8bc661ddf980bc94f83ee9afd
ZENITSUAGATSUMA0 <108956585+ZENITSUAGATSUMA0@users.noreply.github.com>
2022-10-23 03:11:39 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     
     Bot: {
-        token: '', // CLIENT TOKEN
+        token: 'MTAzMzI0NDAxNTE5OTUyMjg4Nw.GrtF1z.RC5BuhurQiPjMHFbLUGwp_DpFk0mAY6riv-jg0', // CLIENT TOKEN
         mongodb: '' // MONGODB APPLICATION URL
     },
 
